1183dcedd09aee861d52f12b7e5bd339437fb030
jinwoong <rassayzsixt@gmail.com>
2024-03-28 16:28:22 +0900

    08.헤더영역 데이터 작업

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <h1 className='header__logo'>
+                <a href="/">
+                    <em>3ik</em>  
+                    <span>Hestia502</span>
+                </a>
+        </h1>
+    )
+}
+
+export default Logo
