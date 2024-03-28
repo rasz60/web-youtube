972c9b6e5102c7905ce9eb193051a54d1842416f
rasz60 <rassayzsixt@gmail.com>
2024-03-29 06:17:32 +0900

    16.슬라이드 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Main from '../components/section/Main'
 import Today from '../components/contents/Today'
 import Developer from '../components/contents/Developer'
-import Webd from '../components/contents/Webd'
-import Website from '../components/contents/Website'
-import Gsap from '../components/contents/Gsap'
-import Portfolio from '../components/contents/Portfolio'
-import Youtube from '../components/contents/Youtube'
+import VideoSlider from '../components/videos/VideoSlider'
+
+import { webdText } from '../data/webd'
+import { websiteText } from '../data/website'
+import { gsapText } from '../data/gsap'
+import { portfolioText } from '../data/portfolio'
+import { youtubeText } from '../data/youtube'
 
 const Home = () => {
     return (
@@ -14,11 +16,11 @@ const Home = () => {
             
             <Today />
             <Developer />
-            <Webd />
-            <Website />
-            <Gsap />
-            <Portfolio />
-            <Youtube />
+            <VideoSlider videos={webdText} title='웹디자인기능사 준비는 이걸로!' id='webd' />
+            <VideoSlider videos={websiteText} title='웹표준 사이트 만들기 기초 다지기' id='wesite' />
+            <VideoSlider videos={gsapText} title='GSAP 패럴랙스 효과를 하고 싶다면!' id='gsap' />
+            <VideoSlider videos={portfolioText} title='포트폴리오 만드는 방법을 공유합니다.' id='portfolio' />
+            <VideoSlider videos={youtubeText} title='지금 이 코딩을 영상으로' id='youtube' />
         </Main>
     )
 }
